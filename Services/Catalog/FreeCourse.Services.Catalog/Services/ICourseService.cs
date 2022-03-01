6cb7bda7aed02fe910577a36f6a1dd56c17cf5a7
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-03-01 23:00:58 +0300

    category & course services are built

--- Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
@@ -3,7 +3,7 @@ using FreeCourse.Shared.Dtos;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace FreeCourse.Services.Catalog.Models
+namespace FreeCourse.Services.Catalog.Services
 {
     public interface ICourseService
     {
