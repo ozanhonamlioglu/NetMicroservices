231100355c4b210dda1b7369072bf9e8f50218c1
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-03-05 01:21:31 +0300

    usercontroller is added

---
+++ IdentityServer/FreeCourse.IdentityServer/Dtos/NoContent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Dtos
+{
+    public class NoContent
+    {
+    }
+}
