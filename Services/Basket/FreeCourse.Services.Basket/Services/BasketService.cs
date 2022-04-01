e6a641375d3db4c5d100dff5a0edab28830e965d
Ozan Honamlioglu <ozanhonamlioglu@hotmail.com>
2022-04-02 01:39:26 +0300

    basket service is done

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Services
+{
+  public class BasketService : IBasketService
+  {
+    private readonly RedisService _readisService;
+
+    public BasketService(RedisService readisService)
+    {
+      _readisService = readisService;
+    }
+
+    public async Task<Response<bool>> Delete(string userId)
+    {
+      var status = await _readisService.GetDb().KeyDeleteAsync(userId);
+      return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket not found", 500);
+    }
+
+    public async Task<Response<BasketDto>> GetBasket(string userId)
+    {
+      var existBasket = await _readisService.GetDb().StringGetAsync(userId);
+
+      if (string.IsNullOrEmpty(existBasket))
+      {
+        return Response<BasketDto>.Fail("Basket not found", 404);
+      }
+
+      return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);
+    }
+
+    public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto, string userId)
+    {
+      var data = new BasketDto() { DiscountCode = basketDto.DiscountCode, UserId = userId, basketItems = basketDto.basketItems };
+      var status = await _readisService.GetDb().StringSetAsync(userId, JsonSerializer.Serialize(data));
+
+      return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket couldn't update or save", 500);
+    }
+  }
+}
